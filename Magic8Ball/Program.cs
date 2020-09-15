24f241a992f2da66afe4bf3fec9286c5dd47b62d
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-15 09:31:55 -0400

    Added more answers and colors

--- Magic8Ball/Program.cs
+++ Magic8Ball/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Magic8Ball
 {
@@ -22,29 +23,84 @@ namespace Magic8Ball
         {
             while(true)
             {
-
+                int numberOfSecondsToSleep = randomObject.Next((5) + 1);
+                Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                 string questionString = GetQuestionFromUser();
-                int randomNumber = randomObject.Next(4);
+
+                if (questionString.ToLower() == "quit")
+                {
+                    break;
+                }
+
+                if (questionString.ToLower() == "bruh")
+                {
+                    Console.WriteLine("This really is a bruh moment");
+                    break;
+                }
+
+                Console.WriteLine("Hmmmmm... let me think...");
+                Thread.Sleep(numberOfSecondsToSleep * 1000);
+                int randomNumber = randomObject.Next(10);
                 switch(randomNumber)
                 {
                     case 0:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("Yes!");
                             break;
                         }
                     case 1:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("No!");
                             break;
                         }
                     case 2:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("Bad Idea!");
                             break;
                         }
                     case 3:
                         {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
                             Console.WriteLine("That's a mystery for the ages!");
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("Maybe!");
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("Perhaps!");
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("Absolutly!");
+                            break;
+                        }
+                    case 7:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("Absolutly not!");
+                            break;
+                        }
+                    case 8:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("Thats not even english!");
+                            break;
+                        }
+                    case 9:
+                        {
+                            Console.ForegroundColor = (ConsoleColor)randomObject.Next(15);
+                            Console.WriteLine("Bruh!");
                             break;
                         }
                 }
