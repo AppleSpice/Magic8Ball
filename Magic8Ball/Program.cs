120786375d06d4f40b181995ac65860bdf543b6a
Gaven Rudiak <gavenrudiak@outlook.com>
2020-09-15 09:05:12 -0400

    It now functions as a magic 8 ball

--- Magic8Ball/Program.cs
+++ Magic8Ball/Program.cs
@@ -4,9 +4,51 @@ namespace Magic8Ball
 {
     class Program
     {
+        static Random randomObject = new Random();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            QuestionLoop();
+        }
+
+        static string GetQuestionFromUser()
+        {
+            Console.Write("What question do you have?");
+            string questionString = Console.ReadLine();
+            return questionString;
+        }
+
+        static void QuestionLoop()
+        {
+            while(true)
+            {
+
+                string questionString = GetQuestionFromUser();
+                int randomNumber = randomObject.Next(4);
+                switch(randomNumber)
+                {
+                    case 0:
+                        {
+                            Console.WriteLine("Yes!");
+                            break;
+                        }
+                    case 1:
+                        {
+                            Console.WriteLine("No!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Bad Idea!");
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("That's a mystery for the ages!");
+                            break;
+                        }
+                }
+            }
         }
     }
 }
